c0a941cffb655cd964f6685d6b9b98f60e5b3f58
Gilang <initial.gr.101@gmail.com>
2020-12-19 01:05:32 +0700

    Finish

---
+++ app/src/main/java/com/app/runningappyt/ui/viewmodels/StatisticsViewModel.kt
@@ -0,0 +1,18 @@
+package com.app.runningappyt.ui.viewmodels
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import com.app.runningappyt.repositories.MainRepository
+import javax.inject.Inject
+
+class StatisticsViewModel @ViewModelInject constructor(
+        val mainRepository: MainRepository
+): ViewModel() {
+
+        val totalTimeRun = mainRepository.getTotalTimeInMillis()
+        val totalDistance = mainRepository.getTotalDistance()
+        val totalCaloriesBurned = mainRepository.getTotalCaloriesBurned()
+        val totalAvgSpeed = mainRepository.getTotalAvgSpeed()
+
+        val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+}
